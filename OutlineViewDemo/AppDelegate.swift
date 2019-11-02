b25a24ec348fe52234bafbe4f07b2086f3b68301
Heaven Chou <heaven.chou@gmail.com>
2019-11-02 15:47:32 +0800

    Initial Commit

---
+++ OutlineViewDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OutlineViewDemo
+//
+//  Created by Heaven Chou on 2019/11/2.
+//  Copyright © 2019 CBETA. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OutlineViewDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OutlineViewDemo
+//
+//  Created by Heaven Chou on 2019/11/2.
+//  Copyright © 2019 CBETA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
